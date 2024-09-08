c27f343c1c9021466b4312c32c8252a8585c9447
Asbelit0 <djasbel10@gmail.com>
2024-09-07 21:46:20 -0500

    ventana calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import *
+
+root=Tk()
+root.title("Calculator")
+root.geometry("570x600+100+200")
+root.resizable(False, False)
+root.configure(bg="#17161b")
+
+
+
+root.mainloop()
